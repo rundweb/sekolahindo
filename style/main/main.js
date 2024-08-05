a5e400dfd0e2e72fbc25f7ba96f937b1822be8c7
faldo <pn4066283@gmail.com>
2024-08-05 14:20:54 +0700

    navbar

---
+++ style/main/main.js
@@ -0,0 +1,6 @@
+var toggleBtn = document.querySelector('.toggle-menu');
+var menuOpen = document.querySelector('.nav-list');
+
+function openMenu() {
+    document.querySelector(".nav-list").classList.toggle("active");
+}
