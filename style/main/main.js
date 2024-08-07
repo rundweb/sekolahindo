acc8354b879d56a1d2cca829c799f58037c8c6af
faldo <pn4066283@gmail.com>
2024-08-07 12:03:03 +0700

    home done

--- style/main/main.js
+++ style/main/main.js
@@ -3,4 +3,9 @@ var menuOpen = document.querySelector('.nav-list');
 
 function openMenu() {
     document.querySelector(".nav-list").classList.toggle("active");
+    document.querySelector(".home-img1").classList.toggle("hiddenimg");
+}
+
+function rightImg() {
+    document.querySelector(".img1").classList.toggle("imgright");
 }
