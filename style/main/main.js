c02d5c52dcd3f0eb8321c1bc8f48de002598f6a3
faldo <pn4066283@gmail.com>
2024-08-09 10:14:20 +0700

    penambahan js scroll

--- style/main/main.js
+++ style/main/main.js
@@ -1,11 +1,21 @@
-var toggleBtn = document.querySelector('.toggle-menu');
-var menuOpen = document.querySelector('.nav-list');
+var toggleBtn = document.querySelector(".toggle-menu");
+var menuOpen = document.querySelector(".nav-list");
 
 function openMenu() {
-    document.querySelector(".nav-list").classList.toggle("active");
-    document.querySelector(".home-img1").classList.toggle("hiddenimg");
+  document.querySelector(".nav-list").classList.toggle("active");
+  document.querySelector(".home-img1").classList.toggle("hiddenimg");
 }
 
 function rightImg() {
-    document.querySelector(".img1").classList.toggle("imgright");
+  document.querySelector(".img1").classList.toggle("imgright");
 }
+
+var scrollNav = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 10) {
+    scrollNav.classList.add("scrollbtn");
+  } else {
+    scrollNav.classList.remove("scrollbtn");
+  }
+});
